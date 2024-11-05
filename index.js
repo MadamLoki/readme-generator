8982bbb7cbff42104f4815701003ac981fe81700
Blake <lokifeathers@gmail.com>
2024-11-05 17:10:23 -0500

    init iquirer and list licenses

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-// TODO: Include packages needed for this application
+// TO DO: Include packages needed for this application
+const inquirer = require('inquirer');
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'list',
+        name: 'favoriteColor',
+        message: 'What is your favorite color?',
+        choices: ['Red', 'Green', 'Blue']
+    }]).then(answers => {
+    console.log(``);
+});
 
 // TODO: Create an array of questions for user input
 const questions = [];
